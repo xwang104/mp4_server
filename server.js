a14fa3ef50fc33503c41cfa0e1ecad369fe4de8e
Xiaoxiao Wang <xiaoxiaowang@Xiaoxiaos-MacBook-Pro.local>
2016-03-27 21:21:20 -0500

    create models and api for users

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // Get the packages we need
 var express = require('express');
 var mongoose = require('mongoose');
-var Llama = require('./models/llama');
+var User = require('./models/user');
+var Task = require('./models/task');
 var bodyParser = require('body-parser');
 var router = express.Router();
 
 //replace this with your Mongolab URL
-mongoose.connect('mongodb://localhost/mp4');
+mongoose.connect('mongodb://xwang104:03131989Wxx@ds051833.mlab.com:51833/mp4');
 
 // Create our Express application
 var app = express();
@@ -37,14 +38,69 @@ homeRoute.get(function(req, res) {
   res.json({ message: 'Hello World!' });
 });
 
-//Llama route
-var llamaRoute = router.route('/llamas');
+//Add more routes here
 
-llamaRoute.get(function(req, res) {
-  res.json([{ "name": "alice", "height": 12 }, { "name": "jane", "height": 13 }]);
+//on routes that end in /users
+var usersRoute = router.route('/users');
+
+usersRoute
+.post(function(req, res) {
+    var user = new User(); 
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.save(function(err) {
+        if (err)
+            res.send(err);
+        res.json({message: 'User created!', data: user});
+    });
+})
+.get(function(req, res) {
+    User.find(function(err, users) {
+        if (err)
+            res.send(err);
+        res.json({message: 'Users obtained!', data: users});
+    })
 });
 
-//Add more routes here
+//on routes that end in /users/:id
+
+var userRoute = router.route('/users/:user_id');
+
+userRoute
+.get(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+        if (err)
+            res.send(err);
+        if (user == null)
+            res.status(404).send('Not found');
+        res.json({message: 'user obtained!', data: user});
+    });
+})
+.put(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+        if (err)
+            res.send(err);
+        if (user == null)
+            res.status(404).send('Not found');
+        user.name = req.body.name;
+        user.email = req.body.email;
+        user.pendingTasks = req.body.pendingTasks;
+        user.save(function(err) {
+            if (err)
+                res.send(err);
+            res.json({message: 'User updated!', data: user});
+        });
+    });
+})
+.delete(function(req, res) {
+    User.findByIdAndRemove(req.params.user_id, function(err, user) {
+        if (err)
+            res.send(err);
+        if (user == null) 
+            res.status(404).send('Not found');
+        res.json({message: 'User successfully deleted', data: user});
+    });
+});
 
 // Start the server
 app.listen(port);
